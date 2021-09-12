eb62efa59e46526677a5c9ae5a24dd22a60cb0aa
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-12 14:15:40 +1000

    Initial import

---
+++ src/main/java/com/matthewcasperson/calendarapi/CalendarapiApplication.java
@@ -0,0 +1,13 @@
+package com.matthewcasperson.calendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarapiApplication.class, args);
+	}
+
+}
