145c921ac642b7adef8c1e66b9857632041c874e
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-13 16:14:31 +1000

    Successfully called graph api

---
+++ src/main/java/com/matthewcasperson/calendarapi/configuration/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.matthewcasperson.calendarapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public static WebClient webClient(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction function =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(oAuth2AuthorizedClientManager);
+        return WebClient.builder()
+                .apply(function.oauth2Configuration())
+                .build();
+    }
+}
--- src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
+++ src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
@@ -1,15 +1,40 @@
 package com.matthewcasperson.calendarapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 
 @RestController
 public class CalendarController {
+
+    private static final String GRAPH_ME_ENDPOINT = "https://graph.microsoft.com/v1.0/me";
+
+    @Autowired
+    private WebClient webClient;
+
     @GetMapping("/calendar")
     public String calendar(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graph) {
-        return graph == null ? "null" : "not null";
+        return callMicrosoftGraphMeEndpoint(graph);
+    }
+
+    private String callMicrosoftGraphMeEndpoint(OAuth2AuthorizedClient graph) {
+        if (null != graph) {
+            String body = webClient
+                    .get()
+                    .uri(GRAPH_ME_ENDPOINT)
+                    .attributes(oauth2AuthorizedClient(graph))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            return body;
+        } else {
+            return "Graph response failed.";
+        }
     }
 }
