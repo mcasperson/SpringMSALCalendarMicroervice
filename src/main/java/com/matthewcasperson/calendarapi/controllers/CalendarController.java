b34d9cf6d72800b51bd8adf88fc2de44dab77dda
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-13 13:43:07 +1000

    Working on resource server

--- src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
+++ src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
@@ -1,31 +1,18 @@
 package com.matthewcasperson.calendarapi.controllers;
 
-import com.matthewcasperson.calendarapi.components.OboAuthProvider;
-import com.microsoft.graph.models.User;
-import com.microsoft.graph.requests.GraphServiceClient;
-import okhttp3.Request;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
 
 @RestController
 public class CalendarController {
-    private OboAuthProvider oboAuthProvider;
 
-    public CalendarController(OboAuthProvider oboAuthProvider) {
-        this.oboAuthProvider = oboAuthProvider;
-    }
 
     @GetMapping("/calendar")
-    public String calendar() {
-        GraphServiceClient<Request> graphClient = GraphServiceClient
-                .builder()
-                .authenticationProvider(oboAuthProvider)
-                .buildClient();
-
-        User user = graphClient.me().buildRequest().get();
-        return user.id;
+    public String calendar(@RegisteredOAuth2AuthorizedClient("graph") OAuth2AuthorizedClient graph) {
+        return graph == null ? "null" : "not null";
     }
 
 
