9489f9388c1dfefa72dfb16c778a01e460fcc891
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-23 17:38:11 +1000

    Fixed imports

--- src/main/java/com/matthewcasperson/calendarapi/configuration/AuthSecurityConfig.java
+++ src/main/java/com/matthewcasperson/calendarapi/configuration/AuthSecurityConfig.java
@@ -3,10 +3,8 @@
 package com.matthewcasperson.calendarapi.configuration;
 
 import com.azure.spring.aad.webapi.AADResourceServerWebSecurityConfigurerAdapter;
-import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class AuthSecurityConfig extends AADResourceServerWebSecurityConfigurerAdapter {
