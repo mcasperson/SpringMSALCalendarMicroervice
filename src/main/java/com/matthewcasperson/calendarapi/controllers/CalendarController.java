de5fb828e553a607f761a7afd406f8312313d083
Matthew Casperson <matthewcasperson@gmail.com>
2021-09-23 10:34:51 +1000

    Made app url generic

--- src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
+++ src/main/java/com/matthewcasperson/calendarapi/controllers/CalendarController.java
@@ -14,7 +14,7 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
 public class CalendarController {
 
     private static final String GRAPH_ME_ENDPOINT = "https://graph.microsoft.com/v1.0/me";
-    private static final String GRAPH_CALENDAR_ENDPOINT = "https://graph.microsoft.com/v1.0/calendar";
+    private static final String GRAPH_CALENDAR_ENDPOINT = "https://graph.microsoft.com/v1.0/me/calendar";
 
     @Autowired
     private WebClient webClient;
@@ -24,10 +24,10 @@ public class CalendarController {
         return callMicrosoftGraphMeEndpoint(graph);
     }
 
-    // https://github.com/Azure-Samples/azure-spring-boot-samples/blob/20910da2b61753d33b3c868e719a940e21117578/aad/azure-spring-boot-starter-active-directory/aad-resource-server-obo/src/main/java/com/azure/spring/sample/aad/controller/SampleController.java
     private String callMicrosoftGraphMeEndpoint(OAuth2AuthorizedClient graph) {
-        System.out.println(graph.getAccessToken().getTokenValue());
         if (null != graph) {
+            System.out.println(graph.getAccessToken().getTokenValue());
+
             String body = webClient
                     .get()
                     .uri(GRAPH_CALENDAR_ENDPOINT)
